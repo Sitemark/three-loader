df6eb2e0fe1cfcda637ab90b73e63f2b417a3826
Maxime Bossens <maxime.bossens@sitemark.com>
2019-08-16 21:38:10 +0200

    Reduce GPU load

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,6 @@ export const DEFAULT_MIN_NODE_PIXEL_SIZE = 50;
 export const DEFAULT_MIN_POINT_SIZE = 2;
 export const DEFAULT_PICK_WINDOW_SIZE = 15;
 export const DEFAULT_POINT_BUDGET = 1_000_000;
-export const MAX_LOADS_TO_GPU = 2;
+export const MAX_LOADS_TO_GPU = 1;
 export const MAX_NUM_NODES_LOADING = 4;
 export const PERSPECTIVE_CAMERA = 'PerspectiveCamera';
